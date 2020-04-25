81ac41b5b50acc629405c5ccfc752233eb63b952
Polat Tuzla <ptuzla@gmail.com>
2020-04-26 00:39:10 +0100

    2.29

---
+++ ch2/2.29b.rkt
@@ -0,0 +1,93 @@
+#lang sicp
+
+; Second implementation
+(define (make-mobile left right)
+  (cons left right))
+
+(define (make-branch length structure)
+  (cons length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cdr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cdr branch))
+
+; only two methods needed to be changed;
+; right-branch and branch-structure
+;
+ (define (total-weight mobile)
+  (if (not (pair? mobile))
+      mobile
+      (let ((l-struct (branch-structure (left-branch mobile)))
+            (r-struct (branch-structure (right-branch mobile))))
+        (+ (if (not (pair? l-struct))
+               l-struct
+               (total-weight l-struct))
+           (if (not (pair? r-struct))
+               r-struct
+               (total-weight r-struct))))))
+
+(define m1 (make-mobile (make-branch 2 9)
+                        (make-branch 1 
+                                     (make-mobile  (make-branch 10
+                                                                (make-mobile  (make-branch 5 3)
+                                                                              (make-branch 3 5)))
+
+                                                   (make-branch 8 10)))))
+;                          m1
+;                          +
+;                          |
+;              2           |           1
+;   +----------------------+-----------------------+
+;   |                                              |
+; +-+-+                                     10     |       8
+; | 9 |                             +--------------+---------------+
+; +---+                             |                              |
+;                                   |                            +-+--+
+;                               5   |     3                      | 10 |
+;                          +--------+---------+                  +----+
+;                          |                  |
+;                        +-+-+              +-+-+
+;                        | 3 |              | 5 |
+;                        +---+              +---+
+; 
+
+(total-weight m1)
+; => 27
+
+
+(define (balanced? mobile)
+  (let ((l-struct (branch-structure (left-branch mobile)))
+        (r-struct (branch-structure (right-branch mobile)))
+        (l-length (branch-length (left-branch mobile)))
+        (r-length (branch-length (right-branch mobile))))
+    (and (= (* (total-weight l-struct) l-length)
+            (* (total-weight r-struct) r-length))
+         (and (if (not (pair? l-struct))
+                  true
+                  (balanced? l-struct))
+              (if (not (pair? r-struct))
+                  true
+                  (balanced? r-struct))))))
+
+(balanced? m1)
+; => #t
+
+(define m2 (make-mobile (make-branch 3 9) ; changed 2 to 3 to make it unbalanced
+                        (make-branch 1 
+                                     (make-mobile  (make-branch 10
+                                                                (make-mobile  (make-branch 5 3)
+                                                                              (make-branch 3 5)))
+
+                                                   (make-branch 8 10)))))
+(balanced? m2)           
+; => #f             
+
+
